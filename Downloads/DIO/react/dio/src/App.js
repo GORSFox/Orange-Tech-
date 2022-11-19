3328c838d49df0fa7722e6f4870c99d73d82ac78
Glaucia Rapozo <gors1315@gmail.com>
2022-11-19 10:43:28 -0300

    add

--- Downloads/DIO/react/dio/src/App.js
+++ Downloads/DIO/react/dio/src/App.js
@@ -1,13 +1,23 @@
-import Button from "./components/Button";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { Home } from './pages/home'
+import { Login } from './pages/login'
 
 function App() {
   return (
-  <div className="App">
-    <h1> Olá React </h1>
-    <Button title="Entrar" />
-   </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+    
+
+    );
 }
 
 export default App;
-
