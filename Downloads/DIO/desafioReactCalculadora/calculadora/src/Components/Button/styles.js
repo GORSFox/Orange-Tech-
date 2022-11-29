6818db07d3d4f583260df5002dc756243930d3d8
Glaucia Rapozo <gors1315@gmail.com>
2022-11-29 16:06:32 -0300

    first commit

---
+++ Downloads/DIO/desafioReactCalculadora/calculadora/src/Components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 2px solid #DB821A;
+    border-radius: 6px;
+    background-color: #111010;
+    color: #FFF9F1;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ Downloads/DIO/desafioReactCalculadora/calculadora/src/Components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  width: 21%;
+  height: 75px;
+  background-color: #111010;
+  border-radius: 6px;
+  border: 1px solid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 40px;
+  font-size: 40px;
+  font-family: comic sans-serif;
+  color: #F09326;
+`;
